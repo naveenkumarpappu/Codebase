006b6bb05184621fd2e42ee1e79763f682fc48d6
naveenkumarpappu <naveenpappu808@gmail.com>
2022-07-09 14:53:15 +0530

    FINAL PROJECT

---
+++ Mini Project/Mproject/Mproject/Data/MprojectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mproject.Model;
+
+namespace Mproject.Data
+{
+    public class MprojectContext : DbContext
+    {
+        public MprojectContext (DbContextOptions<MprojectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mproject.Model.Product> Product { get; set; }
+    }
+}
---
+++ Mini Project/Mproject/Mproject/Model/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mproject.Model
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+
+        public String Brand { get; set; }
+         [DataType(DataType.Date)]
+         [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}",ApplyFormatInEditMode=true)]
+        public DateTime ManufactureDate{get;set;}
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ExpirationDate { get; set; } 
+    }
+}
