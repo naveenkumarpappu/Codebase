1f0f8a209b4cd2c49fe3dc25d8a46ff054e644d6
naveenkumarpappu <naveenpappu808@gmail.com>
2022-07-11 11:16:45 +0530

    Final Project

---
+++ FINAL PROJECT/Final Project/Final Project/Data/Final_ProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Final_Project.Model;
+
+namespace Final_Project.Data
+{
+    public class Final_ProjectContext : DbContext
+    {
+        public Final_ProjectContext (DbContextOptions<Final_ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Final_Project.Model.Employee> Employee { get; set; }
+    }
+}
---
+++ FINAL PROJECT/Final Project/Final Project/Model/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Final_Project.Model
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        [Required(ErrorMessage = "Please Enter First Name" )]
+        public string FirstName { get; set;}
+        public String LastName { get; set; }
+        public string Address { get; set; }
+        public string Location { get; set; }
+    }
+}
